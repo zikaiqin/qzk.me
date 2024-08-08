1df75f420aefd8bdb091f788671ff583d2b1d8fb
zikaiqin <zikaiqin99@gmail.com>
2024-08-08 12:38:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Setup
+
+
+## Dependencies
+
+Requires Node.js version `>16.5.0` to compile.
+
+## Install Packages
+
+Run `npm ci` in this directory.
+
+## Dev Server
+
+Run `npm run dev -- --host` in this directory. This starts a hot-reload server at `localhost:8080`.
+
+
+# Deployment
+
+Coming Soon<sup><small>TM</small></sup>
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/** @type {import('vite').UserConfig} */
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        '404': resolve(__dirname, '404.html'),
+      },
+    },
+  },
+})
